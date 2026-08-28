c50f3ccd30abab4fb589b114731b0963f402c449
spectrumsk <stellak@gmail.com>
2026-08-28 10:27:02 -0700

    Add Ping Pong Training and Training University

--- api/roblox-stats.js
+++ api/roblox-stats.js
@@ -3,19 +3,22 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   res.setHeader('Content-Type', 'application/json');
 
-  const UNIVERSE_IDS = "9807623580,10323220670,9375088027,9540316091,9565343275";
+  const UNIVERSE_IDS = "9807623580,10628526114,10323220670,9375088027,9540316091,9565343275";
   const GROUP_ID = "974814503";
+  const TRAINING_GROUP_ID = "632335719";
 
   try {
-    const [gameResponse, voteResponse, groupResponse] = await Promise.all([
+    const [gameResponse, voteResponse, groupResponse, trainingGroupResponse] = await Promise.all([
       fetch(`https://games.roblox.com/v1/games?universeIds=${UNIVERSE_IDS}`),
       fetch(`https://games.roblox.com/v1/games/votes?universeIds=${UNIVERSE_IDS}`),
-      fetch(`https://groups.roblox.com/v1/groups/${GROUP_ID}`)
+      fetch(`https://groups.roblox.com/v1/groups/${GROUP_ID}`),
+      fetch(`https://groups.roblox.com/v1/groups/${TRAINING_GROUP_ID}`)
     ]);
 
     const gameData = await gameResponse.json();
     const voteData = await voteResponse.json();
     const groupData = await groupResponse.json();
+    const trainingGroupData = await trainingGroupResponse.json();
 
     const games = (gameData.data || []).map(game => {
       const vote = (voteData.data || []).find(v => v.id === game.id);
@@ -31,7 +34,8 @@ export default async function handler(req, res) {
 
     return res.status(200).json({
       games,
-      groupMembers: groupData.memberCount || 0
+      groupMembers: groupData.memberCount || 0,
+      trainingGroupMembers: trainingGroupData.memberCount || 0
     });
 
   } catch (error) {
